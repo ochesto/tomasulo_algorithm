9794a782aa031176ab6219ce2826dbde6d3b1003
Isaac Núñez <shak04995@gmail.com>
2018-05-12 13:27:44 -0600

    Partial implementation issue

--- src/issue.hpp
+++ src/issue.hpp
@@ -6,7 +6,44 @@
 #define TOMASULO_ALGORITHM_ISSUE_HPP
 
 
-class issue {
+#include <string>
+#include <vector>
+#include <boost/algorithm/string.hpp>
+
+class Issue {
+public:
+  struct instr_struct {
+    std::string instr_name;
+    bool issue = false;
+    bool execute = false;
+    bool write_back = false;
+  };
+
+  struct float_register{
+    std::string reg_name = "";
+    std::string reg_value = "";
+  };
+
+  void create();
+
+  void check_issue(std::vector<Issue::instr_struct>& instr_file);
+private:
+  struct reservation_slot {
+      std::string name = "";
+      std::string op = "";
+      std::string qj = "";
+      std::string qk = "";
+      double vj = 0;
+      double vk = 0;
+      int a = 0;
+      bool busy = false;
+  };
+
+  std::vector<reservation_slot> reservation_table;
+  std::vector<float_register> register_status;
+  int total_slot = reservation_table.size();
+
+  std::string checkDependecy(std::string reg);
 
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 
 #include <iostream>
 #include <vector>
+#include "issue.hpp"
 
 int main( int argc, char* argv[] ){
     std::cout << "INICIO" << std::endl;
-    std::vector<std::string> instr_rom;
-    std::string instr;
-    std::cin >> instr;
+    std::vector<Issue::instr_struct> instr_rom;
+    Issue::instr_struct instr;
+    std::cin >> instr.instr_name;
     instr_rom.push_back(instr);
     std::cout << "FIN" << std::endl;
+    Issue issue;
+    issue.create();
 }
