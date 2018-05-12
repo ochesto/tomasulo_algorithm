313e7828257f2793adecabfe3f64b7f9e9cc63a7
Isaac Núñez <shak04995@gmail.com>
2018-05-12 10:50:55 -0600

    Create issue cpp

---
+++ issue.cpp
@@ -0,0 +1,5 @@
+//
+// Created by isaac on 5/12/18.
+//
+
+#include "issue.hpp"
---
+++ issue.hpp
@@ -0,0 +1,14 @@
+//
+// Created by isaac on 5/12/18.
+//
+
+#ifndef TOMASULO_ALGORITHM_ISSUE_HPP
+#define TOMASULO_ALGORITHM_ISSUE_HPP
+
+
+class issue {
+
+};
+
+
+#endif //TOMASULO_ALGORITHM_ISSUE_HPP
