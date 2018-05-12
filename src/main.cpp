c5671b49b6f4dc80fc18478430d9664285b2364b
Isaac Núñez <shak04995@gmail.com>
2018-05-12 11:05:55 -0600

    clean

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 
 #include <iostream>
- using namespace std;
+#include <vector>
 
 int main( int argc, char* argv[] ){
-    cout << "INICIO" << endl;
-    
-    cout << "FIN" << endl;
+    std::cout << "INICIO" << std::endl;
+    std::vector<std::string> instr_rom;
+    std::string instr;
+    std::cin >> instr;
+    instr_rom.push_back(instr);
+    std::cout << "FIN" << std::endl;
 }
